450ea77a4cf271c6c38b472f7ca5ce5ab35ce7d2
shyndavfard <35533714+shyndavfard@users.noreply.github.com>
2018-01-27 11:36:00 +0100

    Add files via upload

---
+++ U12_shayan/src/campusManagement/Semester.java
@@ -0,0 +1,12 @@
+package campusManagement;
+
+
+/**
+ * Enum representing available semesters
+ * 
+ * @author Lukas Roehrig
+ *
+ */
+public enum Semester {
+	WiSe_15_16, SoSe_16, WiSe_16_17, SoSe_17, WiSe_17_18
+}
